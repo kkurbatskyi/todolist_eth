875b68607baed0364eb51f87eb9a341920c8e1e2
kkurbatskyi <57308179+kkurbatskyi@users.noreply.github.com>
2021-12-24 01:29:13 -0800

    Completed front-end

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList{
     uint8 public taskCount = 0;
+
+    struct Task{
+        uint8 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint8 => Task) public tasks;
+
+    constructor() public{
+        createTask("Complete the first task");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
